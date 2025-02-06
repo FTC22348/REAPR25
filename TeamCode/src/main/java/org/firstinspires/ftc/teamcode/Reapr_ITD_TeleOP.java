67392c23a78df3db1b5eeeb4d0817f35fc933463
Typical <avyaykaushik@hotmail.com>
2025-02-06 18:45:24 -0500

    tier 5 crashout

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reapr_ITD_TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reapr_ITD_TeleOP.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 import java.util.List;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.hardware.lynx.LynxModule;
@@ -13,6 +14,12 @@ import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
 public class Reapr_ITD_TeleOp_Copy extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        
+        Gamepad currPad1 = new Gamepad();
+        Gamepad currPad2 = new Gamepad();
+        
+        Gamepad prevPad1 = new Gamepad();
+        Gamepad prevPad2 = new Gamepad();
 
         // Lynx Module, to call both Control Hub and Expansion Hub, reducing the reaction time between yanking the joystick and the robot moving
         List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
@@ -33,20 +40,29 @@ public class Reapr_ITD_TeleOp_Copy extends LinearOpMode {
         motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         
-        // motorFrontLeft.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
-        // motorBackLeft.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
-        // motorFrontRight.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
-        // motorBackRight.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        motorFrontRight.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        
+        // Hanging setup (Driver #1)
+        DcMotor hanger = hardwareMap.get(DcMotor.class, "hang");
         
         // Controls operated by Driver #2
         Servo claw = hardwareMap.get(Servo.class, "claw"); 
         DcMotor arm = hardwareMap.get(DcMotor.class, "arm"); 
         DcMotor fourBarCH = hardwareMap.get(DcMotor.class, "fourBarCH");
         DcMotor fourBarEH = hardwareMap.get(DcMotor.class, "fourBarEH");
+        DcMotor hang = hardwareMap.get(DcMotor.class, "hang");
         Servo wrist = hardwareMap.get(Servo.class, "wrist");
         
-        fourBarEH.setDirection(DcMotorSimple.Direction.REVERSE);
+        boolean hangActive = false;
         
+        Servo arm1 = hardwareMap.get(Servo.class, "arm1");
+        Servo arm2 = hardwareMap.get(Servo.class, "arm2");
+        arm2.setDirection(Servo.Direction.REVERSE);
+        
+        fourBarEH.setDirection(DcMotorSimple.Direction.REVERSE);
         fourBarCH.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
         fourBarEH.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
         
@@ -54,12 +70,15 @@ public class Reapr_ITD_TeleOp_Copy extends LinearOpMode {
 
        if (isStopRequested()) return;
        
-       fourBarCH.setPower(0);
-       fourBarEH.setPower(0);
-       
         double dividePower = 1.0;
 
         while (opModeIsActive()) {
+            prevPad1.copy(currPad1);
+            prevPad2.copy(currPad2);
+            
+            currPad1.copy(gamepad1);
+            currPad2.copy(gamepad2);
+            
             //! Control Speed
             if (gamepad1.left_stick_button || gamepad1.dpad_right) {
                 dividePower = Math.abs(dividePower - 1.0) < 0.1 ? 1.5 : 1.0;
@@ -82,8 +101,8 @@ public class Reapr_ITD_TeleOp_Copy extends LinearOpMode {
             motorBackRight.setPower((y + x - rx) / denominator / dividePower);
 
             // fourBar control stuff            
-            fourBarCH.setPower(gamepad2.left_stick_y);
-            fourBarEH.setPower(gamepad2.left_stick_y);
+            fourBarCH.setPower(-gamepad2.left_stick_y * 0.80);
+            fourBarEH.setPower(-gamepad2.left_stick_y * 0.80);
             
             // move claw open on y if not already at lowest position.
             if (gamepad2.left_trigger > 0.1 || gamepad1.left_trigger > 0.1){
@@ -104,6 +123,38 @@ public class Reapr_ITD_TeleOp_Copy extends LinearOpMode {
             if (gamepad2.x) {
                 arm.setPower(-1);
             }
+            
+            
+            // Hanging stuff
+            if (gamepad1.y) {
+                hanger.setPower(1); // while driver is holding, it goes upwards
+                hanger.setPower(0); // stops as soon as the driver lets go
+            }
+            if (gamepad1.x) {
+                hanger.setPower(-1);
+                sleep(3000); // so it can go downwards
+                hanger.setPower(0); // so the motor doesn't overheat
+            }
+            
+            //this part is currently under revision, don't mess with it avyay
+            if (currPad2.y && !prevPad2.y) {
+                if (arm1.getPosition() == 0.95) {
+                    arm1.setPosition(0.25);
+                    arm2.setPosition(0.25);
+                } else if (arm1.getPosition() == 0.25) {
+                    arm1.setPosition(0.6);
+                    arm2.setPosition(0.6);
+                } else {
+                    arm1.setPosition(0.95);
+                    arm2.setPosition(0.95);
+                }
+            }
+            
+            // if (gamepad1.a) {
+            //     arm.setPower(-1);
+            // } else {
+            //     arm.setPower(1);
+            // }
             
             if (gamepad2.dpad_right) {
                 wrist.setPosition(0.25);
